a8b309821431d357440fc7acc089a1b6ba2cf445
shining-ai <tderkkwii+social@gmail.com>
2024-12-28 13:43:32 +0000

    IPv6対応

--- echo_server/server.c
+++ echo_server/server.c
@@ -35,24 +35,20 @@ int main()
 {
     int socket_fd;
     int new_socket_fd;
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in6 server_addr, client_addr;
     char buffer[BUFFER_SIZE];
     socklen_t addr_len = sizeof(client_addr);
 
-    // socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
 
     // アドレス設定
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin6_family = AF_INET6;
+    server_addr.sin6_addr = in6addr_any;
+    server_addr.sin6_port = htons(PORT);
 
     bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     listen(socket_fd, 1);
     printf("Echo server is running on port %d...\n", PORT);
-
-    // new_socket_fd; = accept(socket_fd, (struct sockaddr *)&client_addr, &addr_len);
-    // printf("Client connected.\n");
 
     while (1)
     {
@@ -66,10 +62,8 @@ int main()
         pthread_create(&tid, NULL, handle_client, client_socket_fd);
 
         pthread_detach(tid);
-
     }
 
     // ソケットを閉じる
-    // close(client_socket_fd);
     close(socket_fd);
 }
