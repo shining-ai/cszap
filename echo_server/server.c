faf497b943cccf558a084d103d6cb85ce55a3510
shining-ai <tderkkwii+social@gmail.com>
2025-01-04 02:45:57 +0000

    サーバの正常終了を実装

--- echo_server/server.c
+++ echo_server/server.c
@@ -3,10 +3,20 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
+#include <signal.h>
 #include <pthread.h>
 #include <stdlib.h>
 #define PORT 8080
 #define BUFFER_SIZE 1024
+volatile int client_num = 0;
+volatile sig_atomic_t server_running = 1;
+int socket_fd;
+
+void handle_sigint(int sig)
+{
+    server_running = 0;
+    close(socket_fd);
+}
 
 void *handle_client(void *client_socket)
 {
@@ -29,32 +39,59 @@ void *handle_client(void *client_socket)
         // クライアントにデータを送り返す
         send(client_socket_fd, buffer, bytes_read, 0);
     }
+    client_num--;
 }
 
 int main()
 {
-    int socket_fd;
     int new_socket_fd;
     struct sockaddr_in6 server_addr, client_addr;
     char buffer[BUFFER_SIZE];
     socklen_t addr_len = sizeof(client_addr);
 
+    signal(SIGINT, handle_sigint);
+
+    // ソケットの作成
     socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (socket_fd < 0)
+    {
+        perror("Error creating socket");
+        return EXIT_FAILURE;
+    }
 
     // アドレス設定
     server_addr.sin6_family = AF_INET6;
     server_addr.sin6_addr = in6addr_any;
     server_addr.sin6_port = htons(PORT);
 
-    bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    listen(socket_fd, 1);
+    // ソケットにアドレスをバインド
+    if (bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error binding socket");
+        close(socket_fd);
+        return EXIT_FAILURE;
+    }
+
+    // クライアントからの接続を待ち受け
+    if (listen(socket_fd, 1) < 0)
+    {
+        perror("Error listening");
+        close(socket_fd);
+        return EXIT_FAILURE;
+    }
     printf("Echo server is running on port %d...\n", PORT);
 
-    while (1)
+    while (server_running)
     {
         printf("Waiting for client...\n");
         new_socket_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &addr_len);
+        if (new_socket_fd < 0)
+        {
+            perror("Error accepting failed");
+            continue;
+        }
         printf("Client connected.\n");
+        client_num++;
 
         pthread_t tid;
         int *client_socket_fd = malloc(sizeof(int));
@@ -62,6 +99,21 @@ int main()
         pthread_create(&tid, NULL, handle_client, client_socket_fd);
 
         pthread_detach(tid);
+    }
+
+    // 新規のクライアント接続を停止
+    printf("Prepare server server stop...\n");
+    int weight_seconds = 10;
+    while (client_num > 0 && weight_seconds > 0)
+    {
+        printf("Waiting for %d clients to disconnect...\n", client_num);
+        sleep(1);
+        weight_seconds--;
+    }
+
+    if (client_num > 0)
+    {
+        printf("Timeout. Force closing all clients...\n");
     }
 
     // ソケットを閉じる
