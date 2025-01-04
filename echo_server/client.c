bc381b532b617c348a7416b8801b3015764e785f
shining-ai <tderkkwii+social@gmail.com>
2025-01-04 06:52:29 +0000

    エラー処理の追加

--- echo_server/client.c
+++ echo_server/client.c
@@ -26,36 +26,62 @@ int main(int argc, char *argv[])
 
     if (argc < 2)
     {
-        printf("Usage: %s <port>\n", argv[0]);
+        printf("Usage: %s <port番号>\n", argv[0]);
         return 1;
     }
     port = atoi(argv[1]);
 
     // ソケットの作成
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0)
+    {
+        perror("Error creating socket");
+        return EXIT_FAILURE;
+    }
 
     // アドレス設定
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
-    inet_pton(AF_INET, "127.0.0.1 ", &server_addr.sin_addr);
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0)
+    {
+        perror("Invalid IP address");
+        close(socket_fd);
+        return EXIT_FAILURE;
+    }
 
     // サーバに接続
-    connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error connecting to server");
+        close(socket_fd);
+        return EXIT_FAILURE;
+    }
     printf("Connected to server.\n");
 
     while (running_client)
     {
         // ユーザーからの入力を受け取る
         printf("Enter message: ");
-        fgets(buffer, BUFFER_SIZE, stdin);
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+        {
+            break;
+        }
 
         // サーバにデータを送信
-        send(socket_fd, buffer, strlen(buffer), 0);
+        if (send(socket_fd, buffer, strlen(buffer), 0) < 0)
+        {
+            perror("Error sending data");
+            break;
+        }
 
         // サーバからのレスポンスを受け取る
-        memset(buffer, 0, BUFFER_SIZE);
+        // memset(buffer, 0, BUFFER_SIZE);
         int bytes_read = recv(socket_fd, buffer, BUFFER_SIZE, 0);
-
+        if (bytes_read < 0)
+        {
+            perror("Error receiving data");
+            break;
+        }
         printf("Received: %s", buffer);
     }
 
